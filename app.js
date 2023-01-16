60c304259811baf4fbc007bca9ba92ebc717dda6
adaDiniz <adacarinadt@gmail.com>
2023-01-16 18:35:22 -0300

    feat: database with mongoose

--- app.js
+++ app.js
@@ -1,26 +1,69 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
 
 const app = express()
-let items = []
 
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
+
+mongoose.set("strictQuery", true)
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+
+const itemsSchema = new mongoose.Schema({
+  name: String
+})
+
+const Item = mongoose.model("Item", itemsSchema)
+
+const itemOne = new Item ({
+  name: "Welcome to your todolist!"
+})
+
+const itemTwo = new Item ({
+  name: "Hit the + button to add a new item."
+})
+
+const itemThree = new Item ({
+  name: "Hit x to delete an item"
+})
+
+const defaultItems = [itemOne, itemTwo, itemThree]
+
+
+
+
 
 app.get('/', function(req, res) {
 
   let today = new Date()
-  
   let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   }
-
   let day = today.toLocaleDateString("en-US", options)
+
+  Item.find({}, function(err, foundItems) {
+
+    if(foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if(err) {
+          console.log("Bug", err)
+        } else {
+          console.log("Done!")
+        }
+      })
+      res.redirect("/")
+    } else {
+      console.log(foundItems)
+      res.render('list', {kindOfDay: day, newListItems: foundItems });
+    }  
+  })
   
-  res.render('list', {kindOfDay: day, newListItems: items });
 })
 
 app.post("/", function(req, res) {
